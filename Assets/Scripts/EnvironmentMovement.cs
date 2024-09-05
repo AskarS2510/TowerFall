894d4548e9e642d185a0bde83116f4c874a1ba9c
Askar <safiullinak@mail.ru>
2024-09-05 14:21:42 +0500

    Добавлено новое окружение

---
+++ Assets/Scripts/EnvironmentMovement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnvironmentMovement : MonoBehaviour
+{
+    [SerializeField] private float _rotateSpeed;
+    [SerializeField] private float _verticalSpeed;
+    [SerializeField] private float _startArg;
+    private Vector3 _startPosition;
+
+    private void Start()
+    {
+        _startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        transform.Rotate(Vector3.up, _rotateSpeed * Time.deltaTime);
+        transform.position = _startPosition + Vector3.up * Mathf.Sin(_startArg + Time.time * _verticalSpeed);
+    }
+}
